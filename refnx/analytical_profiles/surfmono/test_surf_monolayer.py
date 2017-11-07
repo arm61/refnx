0e20958fb1b48e98e9a6f219188421cd56b8a154
arm61 <arm61@bath.ac.uk>
2017-11-07 10:29:39 +0100

    creating of test case

---
+++ refnx/analytical_profiles/surfmono/test_surf_monolayer.py
@@ -0,0 +1,8 @@
+"""The testing module for the surf_monolayer class"""
+
+from numpy.testing import (assert_almost_equal, assert_equal, assert_,
+                           assert_allclose)
+from refnx.reflect import Component
+
+class TestSurfMono(Component):
+
